6074118bb7cba1836202a910914ef1d62f753af5
hania khaled <hendyhania24@gmail.com>
2026-03-25 14:14:09 +0200

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ mlflow.set_experiment("assignment5")
 
 with mlflow.start_run() as run:
     accuracy = random.uniform(0.7, 0.95)
-
     mlflow.log_metric("accuracy", accuracy)
 
     print("Accuracy:", accuracy)
